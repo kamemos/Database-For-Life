e9c2fcb1bcf09ec070191e8c01375e8c315de92c
kamemos <mosagile@gmail.com>
2018-04-18 10:37:18 +0700

    add login function

--- server.js
+++ server.js
@@ -26,11 +26,7 @@ app.use(flash());
 // End Middleware
 
 // Setup Database
-var db = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "root",
-    password: ""
-});
+var db = mysql.createConnection(require('./configs/dbconfig.js'));
 db.connect(function(err) {
     if (err) console.log("err");
     else console.log("Connected!");
@@ -38,10 +34,48 @@ db.connect(function(err) {
 // End Setup Database
 
 // Routing
-app.get('/', function(req, res, next) {
-    res.render('index');
+app.get('/', function(req, res, next){
+    res.render('index',{msg:req.query.err});
+});
+
+app.post('/login', function(req,res,next){
+    if (!req.body.username || !req.body.password){
+        res.redirect('/?err=No username or password');
+    }
+    let user = null;
+    db.query("SELECT * FROM student WHERE Sid="+req.body.username,function (err, result, fields){
+        console.log(result[0].Spassword);
+        if (result && req.body.password === result[0].Spassword){
+            user = result;
+            user.isAuthenticated = true;
+            req.session.user = user[0];
+            res.redirect('/menu')
+        }
+        else {
+            res.redirect('/?err=Username or Password is not correct');
+        }
+    });
+});
+
+app.get('/logout', function(req,res,next){
+    req.session.destroy();
+    res.redirect('/')
+})
+
+app.get('/menu',isLoggedIn, function(req,res,next){
+    console.log(req.session)
+    res.render('menu');
 });
 // End Routing
+
+// Set up middleware to check for login
+function isLoggedIn(req, res, next) {
+    if (req.session.user) {
+      return next();
+    }
+    res.redirect("/");
+}
+
 
 // Create server
 var insecureServer = http.createServer(app).listen(5555, function() {
