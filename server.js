6868f4dbc5af9ca6e3b200cb91e79d249157f4db
induodinum <korpong555king@gmail.com>
2018-04-21 15:43:49 +0700

    Added addsubresult file

--- server.js
+++ server.js
@@ -71,42 +71,75 @@ app.get('/menu',isLoggedIn, function(req,res,next){
     res.render('menu');
 });
 
-<<<<<<< Updated upstream
 app.get('/addsubject',isLoggedIn, function(req,res,next){
     //console.log(req.session.user)
     res.render('addsubject');
 });
 
 app.post('/addsubject',isLoggedIn, function(req,res,next){
-	let subjCode1 = req.body.subjCode1;
-	let subjCode2 = req.body.subjCode2;
-	let secNo1 = req.body.secNo1;
-	let secNo2 = req.body.secNo2;
-	let Sid = req.session.user.Sid;
-	let Syear = 2559;
-	let Ssemester = 2;
-	let Sgrade = "-1";
-	let State = "S";
-	console.log('INSERT INTO Student_registers_in (Sid, Syear, Ssemester, Ssection, Scode, Sgrade, State) VALUES ("'
-		+ Sid + '",' + Syear + ',' + Ssemester + ',' + secNo1 + ',"' + subjCode1 + '","' + Sgrade + '","' + State + '");');
-	db.query('INSERT INTO Student_registers_in (Sid, Syear, Ssemester, Ssection, Scode, Sgrade, State) VALUES ("'
-		+ Sid + '",' + Syear + ',' + Ssemester + ',' + secNo1 + ',"' + subjCode1 + '","' + Sgrade + '","' + State + '");',function(err,result){
-			if(err) throw err;
-			else console.log("insert to database successful");
-	});
-	db.query('INSERT INTO Student_registers_in (Sid, Syear, Ssemester, Ssection, Scode, Sgrade, State) VALUES ("'
-		+ Sid + '",' + Syear + ',' + Ssemester + ',' + secNo2 + ',"' + subjCode2 + '","' + Sgrade + '","' + State + '");',function(err,result,fields){
-			if(err) console.log("insert to database error");
-			else console.log("insert to database successful");
-	});
-	res.redirect('/menu');
+    let subjCode1 = req.body.subjCode1;
+    let subjCode2 = req.body.subjCode2;
+    let secNo1 = req.body.secNo1;
+    let secNo2 = req.body.secNo2;
+    let Sid = req.session.user.Sid;
+    let Syear = 2559;
+    let Ssemester = 2;
+    let Sgrade = "-1";
+    let State = "S";
+    var subjName1 = null;
+    var subjName2 = null;
+    console.log('INSERT INTO Student_registers_in (Sid, Syear, Ssemester, Ssection, Scode, Sgrade, State) VALUES ("'
+        + Sid + '",' + Syear + ',' + Ssemester + ',' + secNo1 + ',"' + subjCode1 + '","' + Sgrade + '","' + State + '");');
+    db.query('INSERT INTO Student_registers_in (Sid, Syear, Ssemester, Ssection, Scode, Sgrade, State) VALUES ("'
+        + Sid + '",' + Syear + ',' + Ssemester + ',' + secNo1 + ',"' + subjCode1 + '","' + Sgrade + '","' + State + '");',function(err,result){
+            if(err) throw err;
+            else console.log("insert to database successful");
+    });
+    db.query('INSERT INTO Student_registers_in (Sid, Syear, Ssemester, Ssection, Scode, Sgrade, State) VALUES ("'
+        + Sid + '",' + Syear + ',' + Ssemester + ',' + secNo2 + ',"' + subjCode2 + '","' + Sgrade + '","' + State + '");',function(err,result){
+            if(err) console.log("insert to database error");
+            else console.log("insert to database successful");
+    });
+    if(subjCode1){
+        console.log('yes subjCode1');
+        db.query('SELECT Sname FROM Student_registers_in natural join Subjects WHERE Sid="' + Sid + '";', function(err,result){
+                if(err) console.log("error query Sname");
+                else if(result.length > 1 ){
+                    console.log('length > 1');
+                    subjName1 = result[0].Sname;
+                    subjName2 = result[1].Sname;
+                    res.render('addsubresult', {
+                        sCode1: subjCode1,
+                        sCode2: subjCode2,
+                        sName1: subjName1,
+                        sName2: subjName2,
+                        sNo1: secNo1,
+                        sNo2: secNo2
+                    });
+                }else if(result.length == 1){
+                    console.log('length = 1');
+                    subjName1 = result[0].Sname;
+                    res.render('addsubresult', {
+                        sCode1: subjCode1,
+                        sCode2: null,
+                        sName1: subjName1,
+                        sNo1: secNo1
+                    });
+                }
+        });
+    }
 });
-=======
+
+
+app.get('/addsubresult',isLoggedIn, function(req,res,next){
+    // console.log(req.flash());
+    res.render('addsubresult');
+});
+
 app.get('/showgrade',isLoggedIn, function(req, res, next){
     res.render('showgrade');
 });
 
->>>>>>> Stashed changes
 // End Routing
 
 // Set up middleware to check for login
